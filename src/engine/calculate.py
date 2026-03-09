d6512a8c1258dc2da955914cf1c58ed84d201e80
Pukiretsu <cr3pym4st3r@gmail.com>
2026-03-09 18:32:08 -0500

    Calculate init

---
+++ src/engine/calculate.py
@@ -0,0 +1,12 @@
+import logging
+from src.utils.configs import Config
+
+logger = logging.getLogger(__name__)
+
+class Engine:
+    def calcular_media_movil(data):
+        pass
+
+
+if __name__ == "__main__":
+    pass
