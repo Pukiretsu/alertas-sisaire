300f7abf325d421fb1c7a56bec8a554173e26b91
Pukiretsu <cr3pym4st3r@gmail.com>
2026-06-16 17:24:09 -0500

    Adding multipage react-router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import 'leaflet/dist/leaflet.css';
 import './index.css';
-import App from './App.jsx';
+import app from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
